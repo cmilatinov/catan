41bf7598962f86ae75477a4938f7ee7237601ad4
Ruben <ruben.e.sanchez.a@gmail.com>
2020-06-14 16:22:13 -0400

    new commit

--- game/settings/SettingsGame.java
+++ game/settings/SettingsGame.java
@@ -39,6 +39,9 @@ public class SettingsGame implements Settings{
     }
 
     public int getBoardRadius() {
+        if(null == null) {
+            System.out.println("fsdf");
+        }
         return boardRadius;
     }
 
